0199a1626e662552448fef37fa981ff53d8aeb18
“Ashish <chaurasiyaashish007@gmail.com>
2025-06-17 17:12:12 +0530

    Cors Fixed

--- src/main/java/com/railswad/deliveryservice/dto/CallbackRequestDTO.java
+++ src/main/java/com/railswad/deliveryservice/dto/CallbackRequestDTO.java
@@ -2,9 +2,11 @@ package com.railswad.deliveryservice.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class CallbackRequestDTO {
     private String name;
     private String email;
